ce38e08669c3ff0517adedc01cbc5ef9c97fb8ab
godofdeath11 <ezmegaz11@gmail.com>
2017-09-25 16:32:34 +0200

    Boards and statuses are getable from data_hander.js functions

--- static/js/data_handler.js
+++ static/js/data_handler.js
@@ -6,29 +6,52 @@
 dataHandler = {
     data: {}, // it contains the boards and their cards and statuses
     loadTestData: function() {
+        localStorage.setItem('data', JSON.stringify(sampleData));
         // uses sampleData from sample_data.js and puts it into the local storage
         // it should be called only once on a test environment
         // To use you have to import the sample_data.js file above this file in your html
     },
+
     loadData: function() {
+        this.data = JSON.parse(localStorage.getItem('data'))
         // loads data from local storage to this.data property
         // this.data = ...
     },
+    
     saveData: function() {
-        // saves the data from this.data to local storage
+        localStorage.setItem('data', JSON.stringify(this.data))
     },
+
     getBoards: function() {
+        this.loadData()
+        return this.data.boards;
         // returns the boards from this.data
     },
     getBoard: function(boardId) {
+        this.loadData()
+        for (var i = 0;i < this.getBoards().length; i++){
+            if (this.getBoards()[i].id == boardId){ 
+                return this.getBoards()[i];
+        }
+        }
         // returns the board with the given id from this.data
     },
+
     getStatuses: function() {
+        this.loadData()
+        return this.data.states;
         // returns the statuses from this.data
     },
+    
     getStatus: function(statusId) {
+        for (var i = 0; i < this.getStatuses().length; i++){
+            if(this.getStatuses()[i].id === statusId){
+                return this.getStatuses()[i];
+            }
+        }
         // returns the status with the given id from this.data
     },
+
     getCardsByBoardId: function(boardId) {
         // returns the cards from this.data which has the given board id
     },
