18f7a6e6a6bbc0b6cd89e823d7871b17a7bf2348
Sz. N. Bálint <szalaynb93@gmail.com>
2017-09-28 17:59:11 +0200

    Properly working edit button without the desired outlook

--- static/js/dom.js
+++ static/js/dom.js
@@ -78,11 +78,11 @@ dom = {
             for (let card_id = 0; card_id < cardList.length; card_id ++){
                 newcard.innerHTML = cardList[card_id].title//<div>title</div>
                 if(cardList[card_id].status_id == target.id.slice(-1)){
-                target.innerHTML += `<div class="card" id=${cardList[card_id].id}>${cardList[card_id].title}
-                                    class="btn btn-xs btn-primary" onclick="dom.editField(${cardList[card_id].id}, ${cardList[card_id].board_id})"
-                                    <span class="glyphicon glyphicon-pencil" >
-                                    </span></div>
-                                    <a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-pencil"></span> edit</a>`
+                target.innerHTML += `<div class="card">
+                                        <div id=${cardList[card_id].id}>${cardList[card_id].title}</div>
+                                        <a href="#" onclick="dom.editField(${cardList[card_id].id}, ${cardList[card_id].board_id})"
+                                            class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-pencil"></span>BUTT</a>
+                                    </div>`
                 }
             }
         }
