ce5655668f3c9025c063fcc47b4f98a5048651c3
godofdeath11 <ezmegaz11@gmail.com>
2017-10-11 10:18:32 +0200

    Deleted useless function in data_handler.js

--- static/js/data_handler.js
+++ static/js/data_handler.js
@@ -5,22 +5,6 @@
 // object itself then you must use the 'this' keyword before. For example: 'this.data' below)
 dataHandler = {
     data: {}, // it contains the boards and their cards and statuses
-    loadTestData: function() {
-        localStorage.setItem('data', JSON.stringify(sampleData));
-        // uses sampleData from sample_data.js and puts it into the local storage
-        // it should be called only once on a test environment
-        // To use you have to import the sample_data.js file above this file in your html
-    },
-
-    loadData: function() {
-        return this.data = JSON.parse(localStorage.getItem('data'))
-        // loads data from local storage to this.data property
-        // this.data = ...
-    },
-    
-    saveData: function() {
-        localStorage.setItem('data', JSON.stringify(this.data))
-    },
 
     getBoards: function(callback) {
         $.ajax({                            
@@ -33,33 +17,6 @@ dataHandler = {
         }
     })
     },
-    
-
-    getBoard: function(boardId) {
-        this.loadData()
-        for (var i = 0;i < this.getBoards().length; i++){
-            if (this.getBoards()[i].id == boardId){ 
-                return this.getBoards()[i];
-        }
-        }
-        // returns the board with the given id from this.data
-    },
-
-    getStatuses: function() {
-        this.loadData()
-        return this.data.states;
-        // returns the statuses from this.data
-    },
-    
-    getStatus: function(statusId) {
-        for (var i = 0; i < this.getStatuses().length; i++){
-            if(this.getStatuses()[i].id === statusId){
-                return this.getStatuses()[i];
-            }
-        }
-        // returns the status with the given id from this.data
-    },
-
     getCardsByBoardId: function(boardId, callback) {
             $.ajax({                            
             url: `/get-cards-by-board-id/${boardId}`,                  
